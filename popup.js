47056a5cf2b43563626396481b3f4c79cb81e3f3
jaga3421 <jagadeesh.jkp@gmail.com>
2024-07-24 19:39:08 +0530

    Upgrade to Manifest 2

---
+++ popup.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var url = tabs[0].url;
+    var qrCode = new QRCode(document.getElementById('qr-code'), {
+      text: url,
+      width: 128,
+      height: 128
+    });
+    document.getElementById('url-text').textContent = url;
+  });
+});
